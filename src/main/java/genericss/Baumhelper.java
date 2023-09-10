3a27ed99e75d55cd73a8534fcc7eb170ff030b7a
Codebreakerrr2 <144541673+Codebreakerrr2@users.noreply.github.com>
2023-09-10 21:09:21 +0200

    Update Baumhelper.java

--- src/main/java/genericss/Baumhelper.java
+++ src/main/java/genericss/Baumhelper.java
@@ -38,15 +38,30 @@ public class Baumhelper {
     }
     /**
      * Die Methode ist eine Hilfsmethode und sucht die Tiefe eines Knotens in einer Baumstruktur.
+     *    Vorbedingung: 
+     * @param knoten darf nicht null sein.
      * Rückgabe:
      * 1. bei Eingabe @param knoten=null wäre die Ausgabe 0;
      * 2. beim nicht finden des Knotens in der Baumstruktur ist die Ausgabe auch 0;
      * sonst immer eine Zahl.
      * */
     private static int tiefenVon(Knoten knoten, Knoten aktuellerKnoten){
+            Preconditions.checkNotNull(knoten,"Knoten ist null!!")
+        if(aktuellerKnoten!=null){
+                if(aktuellerKnoten==knoten){
+                    return 1;
+                } else{
+                    int subTiefe= tiefenVon(knoten,aktuellerKnoten.kind1)+tiefenVon(knoten,aktuellerKnoten.kind2)+tiefenVon(knoten,aktuellerKnoten.kind3);
+                    if(subTiefe==0){
+                        return subTiefe;
+                    }else{
+                        return subTiefe+=subTiefe;
+                    }
+                }
 
-
-
+                
+        }
+return 0;
 
 
     }
