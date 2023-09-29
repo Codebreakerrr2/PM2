2395eff2f59e00f7eb5aaf941b8e3732dfe7de77
Codebreakerrr2 <144541673+Codebreakerrr2@users.noreply.github.com>
2023-09-29 14:20:03 +0200

    Synchronization

---
+++ src/main/java/Synchronisation/ETransporter.java
@@ -0,0 +1,83 @@
+package Synchronisation;
+
+
+
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class ETransporter extends Thread{
+    private boolean threadKontroller=true;
+    private Lagerhaus lagerhaus;
+    private Supplier<String> IDgenerator;
+    /**
+     * DefaultGenerator ist der Default ID Generator, der ID erstellt aus 10 Buchstaben und 5 zahlen aneinander angehängt.
+     * */
+    private Supplier<String> defaultIDGenerator=()-> {
+        Random rand = new Random();
+
+        String stringBuchstaben= Stream.iterate((char) rand.nextInt('a', 'z' + 1), (buchstabe) -> (char) rand.nextInt('a', 'z' + 1)
+        )
+                .limit(10).map(Object::toString).
+                reduce("",(c,e)->c+e);
+       String zahlen= Stream.iterate(rand.nextInt(0,11), zahl->rand.nextInt(0,11)).
+               limit(5).
+               map(String::valueOf).
+               reduce("",(c,e)->c+e);
+return stringBuchstaben+zahlen;
+    };
+
+
+    public ETransporter(Lagerhaus lagerhaus,Supplier<String> IDgenerator){
+        this.lagerhaus=lagerhaus;
+        this.IDgenerator=IDgenerator;
+    }
+
+
+    public ETransporter(Lagerhaus lagerhaus){
+        this.lagerhaus=lagerhaus;
+        this.IDgenerator=defaultIDGenerator;
+    }
+
+
+
+    public void setLagerhaus(Lagerhaus lagerhaus){
+        this.lagerhaus=lagerhaus;
+    }
+
+/**
+ * Die Methode liefert einen Thread der im Lagerhaus Kisten liefert.
+ * */
+    public Thread anliefern(){
+        Runnable task= ()-> {
+        while(threadKontroller){
+            String id=IDgenerator.get();
+            lagerhaus.kisteAnliefern(id);
+        }
+            System.err.println("ETransporter ist aus");
+
+        };
+        Thread thread= new Thread(task);
+        return thread;
+    }
+    /**
+     * Die Methode liefert einen Thread der die Kisten aus dem Lagerhaus holt.
+     * */
+    public Thread kisteAbholen(){
+        Runnable task= ()-> {
+            while(threadKontroller){
+              lagerhaus.kisteAbholen();
+
+            }
+            System.err.println("ETransporter ist aus");
+        };
+        Thread thread= new Thread(task);
+        return thread;
+    }
+    public void transporterOn(){
+        threadKontroller=true;
+    }
+    public void transporterOff(){
+        threadKontroller=false;
+    }
+}
---
+++ src/main/java/Synchronisation/Lagerhaus.java
@@ -0,0 +1,92 @@
+package Synchronisation;
+
+import com.google.common.base.Preconditions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Lagerhaus {
+/**
+ * platzImLager stellt eine struktur für Anzahl der Plätze im Lager dar.
+ * */
+    private List<String> platzImLager;
+    private int anzahlPlatze;
+/**
+ *
+ * */
+    public Lagerhaus(int anzahlPlaetze){
+        this.platzImLager= new ArrayList<>();
+        this.anzahlPlatze=anzahlPlaetze;
+    }
+    /**
+     *Die Methode lagert eine Kiste im lager ab.
+     * Kisten ID darf nicht null sein. sonst @throws Exception
+     * Ist der Lager voll wartet der Thread in der Schlange bis Platz im Lager frei wird.
+     * Das Lagern nimmt 0-500 Millisekunden im Anspruch.
+     * */
+public synchronized void kisteAnliefern(String kistenID){
+    Preconditions.checkNotNull(kistenID,"KistenID darf nicht null sein");
+        if(!hatPlatzFrei()){
+           try{
+            this.wait();
+               System.err.println("Ging In Warte Schleife Thread: "+Thread.currentThread().getName());
+        }   catch (InterruptedException e){
+               Thread.currentThread().interrupt();
+           }
+        }
+        else{
+            platzImLager.add(kistenID);
+            System.err.println("Kiste mit ID: "+kistenID+" wurde gelagert");
+            Random rand= new Random();
+
+            try{
+               Thread.sleep(rand.nextInt(0,501));
+            }   catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+            this.notifyAll();
+            System.err.println("Alle wurden geweckt!");
+        }
+
+
+        }
+        /**
+         * Die Methode entfernt eine Kiste aus dem Lager.
+         * Falls der Lager leer ist wartet der Thread in Warteschlange bis sich eine Kiste im Lager befindet.
+         * @return KistenID wird zurückgegeben.
+         * Return am Ende der Code wird nie erreicht.
+         * */
+        public synchronized String kisteAbholen(){
+                if(platzImLager.isEmpty()){
+                    try{
+                        this.wait();
+                        System.err.println("Ging In Warte Schleife Thread: "+Thread.currentThread().getName());
+                    }   catch (InterruptedException e){
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                else {
+                    String kiste= platzImLager.get(0);
+                    System.err.println("Kiste: "+platzImLager.get(0)+" wurde entfernt");
+                    platzImLager.remove(0);
+                    Random rand= new Random();
+
+                    try{
+                        Thread.sleep(rand.nextInt(0,501));
+                    }   catch (InterruptedException e){
+                        Thread.currentThread().interrupt();
+                    }
+                    this.notifyAll();
+                    System.err.println("Alle wurden geweckt!");
+                    return kiste;
+                }
+                return "Code erreicht das nie!";
+        }
+    /**
+     *Interne Hilfsmethode um zu überprüfen, ob im Lager Platz frei ist.
+     * */
+    private boolean hatPlatzFrei(){
+        return platzImLager.size() < anzahlPlatze;
+    }
+}
